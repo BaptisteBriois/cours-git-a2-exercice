65071fbc55ca07886f7fb1c9e49cd9e87a1439ed
Haissam Chekkal <chekkalhaissam@gmail.com>
2015-10-05 22:29:00 +0200

    Fix #2

--- validation.php
+++ validation.php
@@ -6,7 +6,13 @@ session_start();
 if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && 
 	!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
 
-	// TODO
+	if (isUsernameAvailable($db, $_POST['username']) && isEmailAvailable($db, $_POST['email'])) {
+		userRegistration($db, $_POST['username'], $_POST['email'], $_POST['password']).
+		header('location: login.php');
+	}else{
+		$_SESSION['message'] = 'Erreur : pas dispo';
+		header('Location: register.php');
+	}
 
 }else{ 
 	$_SESSION['message'] = 'Erreur : Formulaire incomplet';
